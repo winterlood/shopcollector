65e43f9fbd0c5edf7063c9becd70eb0866a1e5da
winterlood <king199777@gmail.com>
2021-06-20 11:41:13 +0900

    Update sitemap.xml.tsx

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,10 +1,44 @@
 import React from "react";
 import fs from "fs";
 import { getStoredPostList } from "lib/posts";
+import { renderToStaticMarkup } from "react-dom/server";
 
-const Sitemap = () => {};
+const Sitemap = ({ postPages, staticPages }) => {
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    ${postPages.map((item) => {
+        return `
+        <url>
+        <loc>${item.url}</loc>
+        <lastmod>${item.date}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>1.0</priority>
+      </url>
+        `;
+    })}
+      ${staticPages
+          .map((url) => {
+              return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+          })
+          .join("")}
+    </urlset>
+  `;
+    return (
+        // @ts-ignore
+        // @ts-ignore-start
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>
+        // @ts-ignore-end
+    );
+};
 
-export const getServerSideProps = ({ res }) => {
+export const getServerSideProps = async ({ res }) => {
     const baseUrl = {
         development: "http://localhost:5000",
         production: "https://mydomain.com",
@@ -34,35 +68,9 @@ export const getServerSideProps = ({ res }) => {
         };
     });
 
-    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${postPages.map((item) => {
-        return `
-        <url>
-        <loc>${item.url}</loc>
-        <lastmod>${item.date}</lastmod>
-        <changefreq>monthly</changefreq>
-        <priority>1.0</priority>
-      </url>
-        `;
-    })}
-      ${staticPages
-          .map((url) => {
-              return `
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `;
-          })
-          .join("")}
-    </urlset>
-  `;
-
     res.setHeader("Content-Type", "text/xml");
-    res.write(sitemap);
+    // res.write(sitemap);
+    res.write(renderToStaticMarkup(<Sitemap postPages={postPages} staticPages={staticPages} />));
     res.end();
 
     return {
