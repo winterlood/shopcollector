3eec207845e35dbc7aa0284f0dd81f0cea701b1c
winterlood <king199777@gmail.com>
2021-06-20 16:05:56 +0900

    Update postbuild sitemap generator

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -2,45 +2,49 @@
 const fs = require("fs");
 const path = require("path");
 
+const getDateById = (id, type) => {
+    const rawDate = id.split("@")[0].split("-");
+    const fullDate = rawDate[rawDate.length - 1];
+    const year = fullDate.slice(0, 4);
+    const month = fullDate.slice(4, 6);
+    const day = fullDate.slice(6, 8);
+    if (type === "KOR") {
+        return `${year}년 ${month}월 ${day}일`;
+    } else if (type === "PURE") {
+        return `${year}${month}${day}`;
+    } else {
+        return `${year}-${month}-${day}`;
+    }
+};
+
 function main() {
     const postsDirectory = path.join(process.cwd(), "_posts");
     const fileNames = fs.readdirSync(postsDirectory);
-    console.log(fileNames);
-
-    //     const postsDirectory = path.join(process.cwd(), "_posts");
-    //     const pagesDir = "./.next/server/pages";
-    //     console.log("POST BUILD!");
-
-    //     const pageFiles = getPageFiles(pagesDir);
-
-    //     console.log(pageFiles);
-    //     const idList = pages.map((it) => it.split("_posts/")[1]);
-
-    //     const postPages = idList.map((it) => {
-    //         const id = it.split(".json")[0];
-    //         const date = getDateById(id, "DATE");
-    //         return {
-    //             url: `https://shopcollector.vercel.app/dailybest/${id}`,
-    //             date: new Date(date),
-    //         };
-    //     });
-
-    //     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    //     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    //      ${postPages.map((item) => {
-    //          return `
-    //          <url>
-    //          <loc>${item.url}</loc>
-    //          <lastmod>${item.date}</lastmod>
-    //          <changefreq>monthly</changefreq>
-    //          <priority>1.0</priority>
-    //        </url>
-    //          `;
-    //      })}
-    //     </urlset>
-    //   `;
-
-    //     console.log(postPages);
+    const postPages = fileNames.map((it) => {
+        const id = it.split(".json")[0];
+        const date = getDateById(id, "DATE");
+        return {
+            url: `https://shopcollector.vercel.app/dailybest/${id}`,
+            date: new Date(date),
+        };
+    });
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+         ${postPages.map((item) => {
+             return `
+             <url>
+             <loc>${item.url}</loc>
+             <lastmod>${item.date}</lastmod>
+             <changefreq>monthly</changefreq>
+             <priority>1.0</priority>
+           </url>
+             `;
+         })}
+        </urlset>
+      `;
+    console.log("SITEMAP GENERATED!!!");
+    console.log(sitemap);
+    fs.writeFileSync(path.join("./.next/static", "sitemap.xml"), sitemap);
 }
 
 main();
