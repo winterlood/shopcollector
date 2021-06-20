de40a0e890b309d60f65707c1adb03faa287072b
winterlood <king199777@gmail.com>
2021-06-20 11:26:56 +0900

    [FEAT] : Add ga

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,20 @@ import "styles/Layout.scss";
 import "styles/Footer.scss";
 import "styles/Header.scss";
 import Head from "next/head";
+
+const googleAnalyticsTag = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-EPC17P7MRQ');
+`;
 function MyApp({ Component, pageProps }) {
     return (
         <div>
             <Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-EPC17P7MRQ"></script>
+                <script dangerouslySetInnerHTML={{ __html: googleAnalyticsTag }}></script>
                 <meta name="google-site-verification" content="Crt5iVgpjWMM_wix5Y8ToBQvkjogLpYyYnttvNIeXZ0" />
             </Head>
             <Component {...pageProps} />
