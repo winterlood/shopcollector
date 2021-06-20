1cc4adc800c39b9f26b907d09e5e4d7468f80028
winterlood <king199777@gmail.com>
2021-06-20 11:19:22 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,13 @@ import "styles/product.scss";
 import "styles/Layout.scss";
 import "styles/Footer.scss";
 import "styles/Header.scss";
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
     return (
         <div>
+            <Head>
+                <meta name="google-site-verification" content="Crt5iVgpjWMM_wix5Y8ToBQvkjogLpYyYnttvNIeXZ0" />
+            </Head>
             <Component {...pageProps} />
             <footer></footer>
         </div>
